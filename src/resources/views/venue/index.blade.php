2c6b6842fe58deb0ddf94cc4f98da4f38f78fc02
watya <jyanjyanjyararararaaoao@gmail.com>
2022-05-19 13:42:09 +0900

    css編集

--- src/resources/views/venue/index.blade.php
+++ src/resources/views/venue/index.blade.php
@@ -4,15 +4,22 @@
 
 @section('content')
 
-<h1>会場一覧</h1>
-<div>
-    <a href="{{ route('venue.pia') }}">ぴあアリーナMM</a>
-    <img src="https://pia-arena-mm.jp/wp-content/uploads/2020/07/arena.jpg" width="330" height="250">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+
+<div class="top">
+    <h1>会場一覧</h1>
+    <div>
+        <a href="{{ route('venue.pia') }}">ぴあアリーナMM (神奈川)</a>
+        <a href="{{ route('venue.pia') }}">
+            <img src="https://pia-arena-mm.jp/wp-content/uploads/2020/07/arena.jpg" width="330" height="250">
+        </a>
+    </div>
+    {{-- <div>
+        <a href="{{ route('venue.yokohama') }}">横浜アリーナ</a>
+        <img src="https://nationalstadium-tours.com/wp-content/uploads/2020/06/IMG_0633-2000x1200.jpg" width="330"
+            height="250">
+    </div> --}}
+
 </div>
-{{-- <div>
-    <a href="{{ route('venue.yokohama') }}">横浜アリーナ</a>
-    <img src="https://nationalstadium-tours.com/wp-content/uploads/2020/06/IMG_0633-2000x1200.jpg" width="330"
-        height="250">
-</div> --}}
 
 @endsection
