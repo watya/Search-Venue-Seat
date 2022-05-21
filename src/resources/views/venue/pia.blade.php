1cc89229b8c878278bc10c8466f4152a0c96e1fe
watya <jyanjyanjyararararaaoao@gmail.com>
2022-05-21 12:37:10 +0900

    縦画面

--- src/resources/views/venue/pia.blade.php
+++ src/resources/views/venue/pia.blade.php
@@ -2,34 +2,6 @@
 
 @section('title', 'ライブ座席検索')
 
-<script type="text/javascript">
-    2
-    window.onorientationchange = function () {
-    3
-     switch ( window.orientation ) {
-    4
-      case 0:
-    5
-       break;
-    6
-      case 90:
-    7
-       alert('画面を縦にしてください');
-    8
-       break;
-    9
-      case -90:
-    10
-       alert('画面を縦にしてください');
-    11
-       break;
-    12
-     }
-    13
-    }
-    14
-</script>
-
 <link rel="stylesheet" href="{{ asset('css/pia.arena.css') }}">
 <link rel="stylesheet" href="{{ asset('css/pia.standL.css') }}">
 <link rel="stylesheet" href="{{ asset('css/pia.standR.css') }}">
