ca0d12c2012e8d384c04b06b36a47742b5c8b643
watya <jyanjyanjyararararaaoao@gmail.com>
2022-05-21 12:34:23 +0900

    縦画面

--- src/resources/views/venue/pia.blade.php
+++ src/resources/views/venue/pia.blade.php
@@ -1,23 +1,6 @@
 @extends('layouts.app')
 
 @section('title', 'ライブ座席検索')
-<link rel="stylesheet" href="{{ asset('css/pia.arena.css') }}">
-<link rel="stylesheet" href="{{ asset('css/pia.standL.css') }}">
-<link rel="stylesheet" href="{{ asset('css/pia.standR.css') }}">
-<link rel="stylesheet" href="{{ asset('css/pia.standC.css') }}">
-<link rel="stylesheet" href="{{ asset('css/pia.standLC.css') }}">
-<link rel="stylesheet" href="{{ asset('css/pia.standRC.css') }}">
-
-
-@section('content')
-
-<div class="component">
-    <pia-component></pia-component>
-</div>
-
-@endsection
-
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 <script type="text/javascript">
     2
@@ -46,3 +29,21 @@
     }
     14
 </script>
+
+<link rel="stylesheet" href="{{ asset('css/pia.arena.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pia.standL.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pia.standR.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pia.standC.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pia.standLC.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pia.standRC.css') }}">
+
+
+@section('content')
+
+<div class="component">
+    <pia-component></pia-component>
+</div>
+
+@endsection
+
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
